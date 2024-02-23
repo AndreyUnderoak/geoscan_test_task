0c04b911414ba3cfaf2636ccc82187763b31f3cc
Andrey Underoak <eaa4robo@gmail.com>
2024-02-23 05:06:48 -0800

    Add normalize

--- task_workspace/ImgWorker.py
+++ task_workspace/ImgWorker.py
@@ -1,11 +1,15 @@
 import cv2
-from ToGray import ToGray
-import glob as gl
 import numpy as np
 
 class ImgWorker:
     def save_img(imgs, name):
         for i in range(imgs.shape[0]):
+            # imk = imgs[i]
+            # rows,cols = imk.shape
+            # for i2 in range(rows):
+            #     for j in range(cols):
+            #         k = imk[i2,j]
+            #         print(k)
             cv2.imwrite('output_images/' + str(name) + "_" +str(i) +'.png', imgs[i])
 
     def smooth_resize(imgs, px):
@@ -35,3 +39,8 @@ class ImgWorker:
             ret_data.append(cv2.Sobel(imgs[i], ddepth=cv2.CV_32F, dx=0, dy=1))
         return np.asarray(ret_data)
 
+    def normalize(imgs):
+        ret_data = []
+        for i in range(imgs.shape[0]):
+            ret_data.append(cv2.normalize(imgs[i], None, 0, 100.0, cv2.NORM_MINMAX, dtype=cv2.CV_32F))
+        return np.asarray(ret_data)
