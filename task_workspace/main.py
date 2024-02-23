88202de1c30c0776f352b1618e1a338aaf4c78dc
Andrey Underoak <eaa4robo@gmail.com>
2024-02-23 05:06:14 -0800

    Use my model

--- task_workspace/main.py
+++ task_workspace/main.py
@@ -1,109 +1,41 @@
 import cv2
+from ToGray import ToGray
+from ImgWorker import ImgWorker
 import glob as gl
 import numpy as np
-import matplotlib.pyplot as plt
-from matplotlib.pyplot import savefig
-import tensorflow.compat.v1 as tf
-
-tf.disable_v2_behavior()
 
 
-path = "./dataset/*.png"
+model_px = 128
+goal_px = 2048
+path = "./input_images/*.png"
+train = False
 
 # count = 0
-# filenames = gl.glob(path)
-# for filename in filenames:
-#     image = cv2.imread(filename)
-#     color_img = cv2.GaussianBlur(cv2.resize(image, (2048, 2048)),(5,5),0)
-#     gray_img = cv2.cvtColor(color_img, cv2.COLOR_BGR2GRAY)
+filenames = gl.glob(path)
 
-#     count += 1
-#     print("Graying image " + str(count))
+if(train):
+    pass
 
-#     cv2.imwrite("./gray_images/gray_" +str(count) +".png", gray_img)
-#     cv2.imwrite("./color_images/color_" +str(count) +".png", color_img)
+#Init saved model
+tg = ToGray("./pretrained/ColorToGray.ckpt-49", model_px)
 
-count = 200
+input_images = ImgWorker.load_img(filenames, model_px)
 
-dataset = []
+input_images_np = np.asarray(input_images)   
 
-#Read all color images and append into numpy list
-for i in range(1, count):
-    img = cv2.imread("color_images/color_" +str(i) +".png" )
-    dataset.append(np.array(img))
+gray_images = tg.color_to_gray_array(input_images)
+smoothed_images = ImgWorker.smooth_resize(gray_images, goal_px)
 
-dataset_source = np.asarray(dataset)
-print(dataset_source.shape)
+x_grad = ImgWorker.to_grad_x(smoothed_images)
+y_grad = ImgWorker.to_grad_y(smoothed_images)
 
-dataset_tar = []
+x_norm = ImgWorker.normalize(x_grad)
+y_norm = ImgWorker.normalize(y_grad)
 
-#Read all grayscale images and append into numpy list
-for i in range(1, count):
-    img = cv2.imread("gray_images/gray_" +str(i) +".png", 0)    
-    dataset_tar.append(np.array(img))
-
-dataset_target = np.asarray(dataset_tar)
-print(dataset_target.shape)
+ImgWorker.save_img(x_norm, "x_norm")
+ImgWorker.save_img(y_norm, "y_norm")
 
 
-def autoencoder(inputs):
-
-    # Encoder
-    net = tf.layers.conv2d(inputs, 2048, 2, activation = tf.nn.relu)
-    print(net.shape)
-    net = tf.layers.max_pooling2d(net, 2, 2, padding = 'same')
-    print(net.shape)
 
 
-    # Decoder
-    net = tf.image.resize_nearest_neighbor(net, tf.constant([2049, 2049]))
-    net = tf.layers.conv2d(net, 1, 2, activation = None, name = 'outputOfAuto')
-    print("NET SHAPE")
-    print(net.shape)
-
-    return net
-
-
-ae_inputs = tf.placeholder(tf.float32, (None, 2048, 2048, 3), name = 'inputToAuto')
-ae_target = tf.placeholder(tf.float32, (None, 2048, 2048, 1))
-
-ae_outputs = autoencoder(ae_inputs)
-lr = 0.001 # learning rate 
-
-loss = tf.reduce_mean(tf.square(ae_outputs - ae_target))
-train_op = tf.train.AdamOptimizer(learning_rate = lr).minimize(loss)
-init = tf.global_variables_initializer() #This will be used to run a session for training
-
-# define the constant variables
-batch_size = 32
-epoch_num = 50
-saving_path = "pretrained/AutoencoderColorToGray.ckpt"
-saver_ = tf.train.Saver(max_to_keep = 3)
-
-# input data to the network is stored in variables
-batch_img = dataset_source[0:batch_size]
-batch_out = dataset_target[0:batch_size]
-num_batches = count//batch_size
-
-# Create a session object and run the global variable which was defined earlier
-sess = tf.Session()
-sess.run(init)
-
-# new images are sent into the network in batches of 32
-for ep in range(epoch_num):
-    batch_size = 0
-    for batch_n in range(num_batches): # batches loop
-
-        # runs the computational graph in the autoencoder with the given input data and target data
-        _, c = sess.run([train_op, loss], feed_dict = {ae_inputs: batch_img, ae_target: batch_out})
-        print("Epoch: {} - cost = {:.5f}" .format((ep+1), c))
-        batch_img = dataset_source[batch_size: batch_size+32]
-        batch_out = dataset_target[batch_size: batch_size+32]
-        batch_size += 32
-
-    saver_.save(sess, saving_path, global_step = ep)
-
-sess.close()
-
-print("READY")
-
+cv2.waitKey(0)
